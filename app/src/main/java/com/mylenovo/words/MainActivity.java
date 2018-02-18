baeee5aeeebaf71552a7c5ae720c2c23a5a441a4
Tanmay Srivastava <tanmays321@gmail.com>
2018-02-19 02:04:28 +0530

    First

--- app/src/main/java/com/mylenovo/words/MainActivity.java
+++ app/src/main/java/com/mylenovo/words/MainActivity.java
@@ -1,5 +1,7 @@
 package com.mylenovo.words;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder aBuilder = new AlertDialog.Builder( this );
+        aBuilder.setMessage( "Want to EXIT?" ).setCancelable( false ).setPositiveButton( "Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                moveTaskToBack( true );
+                android.os.Process.killProcess( android.os.Process.myPid() );
+                System.exit( 1 );
+            }
+        } ).setNegativeButton( "No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        } );
+        AlertDialog alertDialog = aBuilder.create();
+        alertDialog.show();
     }
 }
--- app/src/main/java/com/mylenovo/words/Splash_Activity.java
+++ app/src/main/java/com/mylenovo/words/Splash_Activity.java
@@ -1,7 +1,8 @@
 package com.mylenovo.words;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class Splash_Activity extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class Splash_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_);
+        getSupportActionBar().hide();
+        new Thread( new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep( 5000 );
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent in = new Intent( Splash_Activity.this,MainActivity.class );
+                startActivity( in );
+            }
+        } ).start();
     }
 }
